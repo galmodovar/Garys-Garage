a8a3e9ff5a2ef38a265baee4fb93234664724fbc
galmodovar <galmodovar1@gmail.com>
2022-08-27 11:43:57 -0500

    combining props, adding vehicle class

--- Cessna.cs
+++ Cessna.cs
@@ -2,11 +2,9 @@ using System;
 
 namespace Garage
 {
-    public class Cessna  // Propellor light aircraft
+    public class Cessna : Vehicle  // Propellor light aircraft
     {
         public double FuelCapacity { get; set; }
-        public string MainColor { get; set; }
-        public int MaximumOccupancy { get; set; }
 
         public void RefuelTank()
         {
--- Zero.cs
+++ Zero.cs
@@ -2,11 +2,9 @@ using System;
 
 namespace Garage
 {
-    public class Zero  // Electric motorcycle
+    public class Zero : Vehicle // Electric motorcycle
     {
         public double BatteryKWh { get; set; }
-        public string MainColor { get; set; }
-        public int MaximumOccupancy { get; set; }
 
         public void ChargeBattery()
         {
